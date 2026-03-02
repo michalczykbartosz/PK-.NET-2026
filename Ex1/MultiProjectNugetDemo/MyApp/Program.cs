760b471f0aaad49491d0ea82eae1e38d3d086458
michalczykbartosz <bart3q747@gmail.com>
2026-03-02 11:59:37 +0100

    Wykonanie zadania nr.1

---
+++ Ex1/MultiProjectNugetDemo/MyApp/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using MyLibrary;
+using MyServices;
+using Newtonsoft.Json;
+using Microsoft.Extensions.DependencyInjection;
+
+var kalkulator = new Calculator();
+int wynik_dod = kalkulator.Add(3,5);
+int wynik_odej = kalkulator.Subtract(3, 5);
+Console.WriteLine($"Wynik dodawania 3+5 = {wynik_dod}");
+Console.WriteLine($"Wynik odemowania 3-5 = {wynik_odej}");
+
+int sum = kalkulator.Add(5, 3);
+var result = new { Operation = "Add", A = 5, B = 3, Result = sum };
+string jsonResult = JsonConvert.SerializeObject(result, Formatting.Indented);
+Console.WriteLine(jsonResult);
+
+// Konfiguracja kontenera DI
+var serviceProvider = new ServiceCollection()
+    .AddSingleton<ILoggerService, ConsoleLogger>()
+    .BuildServiceProvider();
+
+// Uzyskanie instancji loggera
+var logger = serviceProvider.GetService<ILoggerService>();
+logger.Log("Aplikacja uruchomiona.");
+
+// Przykładowe użycie kalkulatora
+int sum2 = kalkulator.Add(10, 15);
+logger.Log($"Wynik dodawania: {sum2}");
+
---
+++ Ex1/MultiProjectNugetDemo/MyServices/Class1.cs
@@ -0,0 +1,14 @@
+﻿namespace MyServices;
+
+public interface ILoggerService
+{
+    void Log(string message);
+}
+
+public class ConsoleLogger : ILoggerService
+{
+    public void Log(string message)
+    {
+        Console.WriteLine($"[LOG]: {message}");
+    }
+}
